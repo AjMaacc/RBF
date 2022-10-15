e2aba2f98253e91527f2573fc86fb48455dc7cb4
AjMac <aj03110@gmail.com>
2022-10-15 01:29:24 -0400

    Initial commit

---
+++ src/main/java/me/ajmac/contactflag/ContactFlag.java
@@ -0,0 +1,66 @@
+package me.ajmac.contactflag;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+public final class ContactFlag extends JavaPlugin implements Listener {
+
+    public static StateFlag CONTACT_DAMAGE;
+    WorldGuardPlugin worldguard;
+
+    public void onEnable() {
+        getLogger().log(Level.INFO, "RBF - Made by AjMaacc");
+        // Plugin startup logic
+        Plugin wgPlugin = getServer().getPluginManager().getPlugin("WorldGuard");
+        if (wgPlugin instanceof WorldGuardPlugin) {
+            worldguard = (WorldGuardPlugin) wgPlugin;
+        }
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @Override
+    public void onLoad() {
+        WorldGuard worldGuard = WorldGuard.getInstance();
+        FlagRegistry flagRegistry = worldGuard.getFlagRegistry();
+
+        flagRegistry.register(CONTACT_DAMAGE = new StateFlag("contact-damage", true));
+        getLogger().log(Level.INFO, "Loaded contact flag successfully!");
+    }
+    @EventHandler
+    public void onContactDamage(EntityDamageByBlockEvent e) {
+
+        if (e.getEntity() instanceof Player){
+            LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer((Player) e.getEntity());
+            Location location = e.getEntity().getLocation();
+            RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+            ApplicableRegionSet set = container.get(new BukkitWorld(location.getWorld())).getApplicableRegions(BlockVector3.at(location.getX(),location.getY(),location.getZ()));
+
+            if (e.getDamager().getType().equals(Material.SWEET_BERRY_BUSH) && !(set.testState(localPlayer, ContactFlag.CONTACT_DAMAGE)))
+                e.setCancelled(true);
+        }
+    }
+
+}
